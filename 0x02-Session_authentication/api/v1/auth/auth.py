e490139694b94811825fbbd3cc285033826a157c
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-05-04 10:23:18 +0100

    Authorization

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Contains Auth class which is the base class for
+all authentication methods
+"""
+from typing import List
+from typing import TypeVar
+from os import getenv
+
+
+class Auth:
+    """
+    Base class for authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Finds for the path in the excluded_paths list.
+
+        Args:
+            path: The path to check.
+            excluded_paths: The list of paths to exclude.
+
+        Returns:
+            True if the path is not in the excluded_paths list.
+            True path is None or excluded_paths is None or empty.
+            False otherwise.
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        if path not in excluded_paths:
+            return True
+
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Determines if a request has valid authorization headers.
+
+        Args:
+            request: The request to check.
+
+        Returns:
+            The authorization header if it exists.
+            None otherwise.
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Determines if a request has a valid user.
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """
+        Retrieves the session cookie from the request.
+
+        Args:
+            request: The request to check.
+
+        Returns:
+            The session cookie if it exists.
+            None otherwise.
+        """
+        if not request:
+            return None
+        _my_session_id = getenv('SESSION_NAME')
+        return request.cookies.get(_my_session_id, None)
